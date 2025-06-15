160f89146ec410eaecf769847ea5f663a03d5356
Austin Elke <103085481+TheGiantTreetrunk@users.noreply.github.com>
2025-06-14 21:17:44 -0700

    ...
    ...

--- y/app.js
+++ y/app.js
@@ -1,19 +1,47 @@
 document.addEventListener("DOMContentLoaded", event => {
 
-    const app = firebase.app();
-    console.log(app);
-});
+      const app = firebase.app();
+      console.log(app);
+      });
 
-function googleLogin(){
-    const provider = new firebase.auth.GoogleAuthProvider();
+    var usrn = "";
+    function googleLogin(){
+        const provider = new firebase.auth.GoogleAuthProvider();
 
-    firebase.auth().signInWithPopup(provider)
+        firebase.auth().signInWithPopup(provider)
 
-    .then(result => {
-        const user = result.user;
-        document.write('Hello and welcome!');
-        document.getElementById(test).innerHTML = "Welcome!!!!";
-        console.log(user)
-    })
-    .catch(console.log)
+        .then(result => {
+            const user = result.user;
+            //document.write('Hello and welcome! ${user.displayName}' + user.displayName)
+            
+            console.log(user)
+            usrn = user.displayName
+            Update()
+        })
+        .catch(console.log);
+
+        
+    }
+var plyr_dd = {
+    Alignment: "Evil",
+    Armor: 0,
+    Deaths: 0,
+    Food: 1,
+    Gold: 0,
+    Health: 10,
+    Kills: 0,
+    Level: 1,
+    Tag: "",
+    Weapon: ""
+};
+function Update() {
+  document.getElementById("test").innerHTML = "Welcome!!!!" + usrn;
+  document.getElementById("sgn_in").style.display = "none";
+
+  const db = firebase.firestore();
+  const myacc = db.collection('User').doc(usrn);
+  myacc.onSnapshot(doc => {
+    const data = doc.data();
+    plyr_dd.Alignment = data.Alignment;
+  })
 }
